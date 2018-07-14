442aca5d07f5cd95b94c9f84730bba22f6522daf
Naina Kapil <nainakapil22@gmail.com>
2018-07-15 02:29:34 +0530

    retrived data from json file and some more small changes.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ window.onload = function () {
 
     var mymap = L.map('map').setView([24.6005, 80.8322], 6);
     L.tileLayer('https://api.mapbox.com/styles/v1/nainakapil/cjj2qpm2e2p672sobazzcf0dz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFpbmFrYXBpbCIsImEiOiJjamoxN3FvZHgwbDB4M2tud3RlN2o3dGhiIn0.HdZy3o_hp7PerOE8Op1dDw', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 15,
         minZoom: 3,
         id: 'mapbox.streets',
@@ -11,9 +10,9 @@ window.onload = function () {
 
     // moving icon set as starfish
     var Icon = L.icon({
-        iconUrl: 'assets/img/fish.png',
-        iconSize: [50, 50],
-        IiconAnchor: [25, 25],
+        iconUrl: 'assets/img/micon.png',
+        iconSize: [60, 60],
+        IiconAnchor: [45, 45],
     });
 
     // marker set as blue light image
@@ -51,46 +50,65 @@ window.onload = function () {
         maxWidth: 5
     }).addTo(mymap);
 
-    // for loop for showing markers(blue led) and popups at coordinates mentioned in dataSet
-    for (data in dataSet) {
+    $.getJSON('js/gangacoord.json', function (dataSet) {
+        // for loop for showing markers(blue led) and popups at coordinates mentioned in dataSet
+        for (data in dataSet) {
 
-        var dataMarker = L.marker(dataSet[data].coord, {
-            icon: lightIcon
-        }).addTo(mymap);
-        // we can apply html to popup content using variable popupContent
-        var waterLevel =dataSet[data].waterLevel.toString();
-        var waterFlow =dataSet[data].waterFlow.toString();
-        var popupContent = `
-        <div class="row">
-            <div class="col-6">
-                <img src="assets/img/sea.png"/> 
-                <div>Water Level: ${waterLevel}</div>
-            </div>
-            <div class="col-6">
-                <img src="assets/img/wind.png"/> 
-                <div>Water Flow: ${waterFlow}</div>
-            </div>
-        </div>`;
-        // defined class for popup is customPopup
-        dataMarker.bindPopup(popupContent, {
-            className: "customPopup",
-            minWidth: 300,
-            maxWidth: 1000
+            var dataMarker = L.marker(dataSet[data].coord, {
+                icon: lightIcon,
+                zIndexOffset: 1000
+            }).addTo(mymap);
+            // we can apply html to popup content using variable popupContent
+            var waterLevel = dataSet[data].waterLevel.toString();
+            var waterFlow = dataSet[data].waterFlow.toString();
+            var waterQuality = dataSet[data].waterQuality.toString();
+            var popupContent = `
+                <div class="row">
+                    <div class="col-sm-4 popup-item">
+                        <img src="assets/img/level.png"/> 
+                        <div class="popup-text">
+                            <div>Water Level</div>
+                            <div>${waterLevel}</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 popup-item">
+                        <img src="assets/img/flow.png"/> 
+                        <div class="popup-text">
+                            <div>Water Flow</div>
+                            <div>${waterFlow}</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 popup-item">
+                        <img src="assets/img/chemistry.png"/> 
+                        <div class="popup-text">
+                            <div>Water Quality</div>
+                            <div>${waterQuality}</div>
+                        </div>
+                    </div>
+                </div>
+            `;
 
-        });
+            // defined class for popup is custom-popup
+            dataMarker.bindPopup(popupContent, {
+                className: "custom-popup",
+                minWidth: 350,
+                maxWidth: 400
 
-        // popup appears on mouse hover
-        // dataMarker.on('mouseover', function (e) {
-        //     this.openPopup();
-        // });
-        // dataMarker.on('mouseout', function (e) {
-        //     this.closePopup();
-        // });
+            });
 
-    }
+            // popup appears on mouse hover
+            dataMarker.on('mouseover', function (e) {
+                this.openPopup();
+            });
+            // dataMarker.on('mouseout', function (e) {
+            //     this.closePopup();
+            // });
+        }
+    });
+}
 
 
-};
+
 var latLngs = [
     [30.1459, 78.5993],
     [30.0869, 78.2676],
@@ -100,55 +118,4 @@ var latLngs = [
     [25.3176, 82.9739],
     [25.5941, 85.1376],
     [22.5726, 88.3639]
-];
-
-// Array of JSON objects
-var dataSet = [{
-        "coord": [30.1459, 78.5993],
-        "waterLevel": 50,
-        "waterFlow": 12,
-        "waterQuality": 2
-    },
-    {
-        "coord": [30.0869, 78.2676],
-        "waterLevel": 60,
-        "waterFlow": 18,
-        "waterQuality": 1
-    },
-    {
-        "coord": [29.9457, 78.1642],
-        "waterLevel": 40,
-        "waterFlow": 8,
-        "waterQuality": 0
-    },
-    {
-        "coord": [26.4499, 80.3319],
-        "waterLevel": 30,
-        "waterFlow": 22,
-        "waterQuality": 1
-    },
-    {
-        "coord": [25.4358, 81.8463],
-        "waterLevel": 10,
-        "waterFlow": 20,
-        "waterQuality": 3
-    },
-    {
-        "coord": [25.3176, 82.9739],
-        "waterLevel": 18,
-        "waterFlow": 70,
-        "waterQuality": 2
-    },
-    {
-        "coord": [25.5941, 85.1376],
-        "waterLevel": 8,
-        "waterFlow": 7,
-        "waterQuality": 1
-    },
-    {
-        "coord": [22.5726, 88.3639],
-        "waterLevel": 18,
-        "waterFlow": 70,
-        "waterQuality": 0
-    },
 ];
